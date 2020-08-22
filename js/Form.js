7a2d24c49c02d59a25d7ed28762971934a5fca4c
Shaun <sheenamaryt@gmail.com>
2020-08-22 13:56:45 +0530

    First commits

---
+++ js/Form.js
@@ -0,0 +1,52 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton('Reset');
+  }
+
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  show(){
+    this.reset.show();
+  }
+
+  display(){
+
+    this.title.html("Car Racing Game!");
+    this.title.position(displayWidth/2 - 75, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 25, displayHeight/2);
+    this.reset.position(displayWidth-75,20);
+    this.reset.hide();
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name + ", please wait as the other players join")
+      this.greeting.position(displayWidth/2 - 200, displayHeight/4);
+    });
+
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+      Player.updateCarsAtEnd(0);
+      Player.updatePlayers();
+    });
+
+  }
+}
